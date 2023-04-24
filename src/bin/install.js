1803664ce190b1ff238ae61d029ed93d360165da
John Deighan <john.deighan@protonmail.com>
2023-04-24 10:29:46 -0400

    debug install

--- src/bin/install.js
+++ src/bin/install.js
@@ -43,29 +43,33 @@ catch (err) {
 	process.exit(1);
 	}
 
-try {
-	console.log('Cloning...');
-//	spawnSync(`git clone --depth 1 ${git_repo} ${newDirPath}`);
-	run_cmd('git' ['clone','--depth','1',git_repo, newDirPath]);
+async function main() {
+	try {
+		console.log('Cloning...');
+	//	spawnSync(`git clone --depth 1 ${git_repo} ${newDirPath}`);
+		await run_cmd('git' ['clone', '--depth', '1', git_repo, newDirPath]);
 
-	// --- Change directory
-	process.chdir(newDirPath);
+		// --- Change directory
+		process.chdir(newDirPath);
 
-	// --- Install dependencies
-	console.log('Installing dependencies...');
-//	spawnSync('npm install');
-	run_cmd('npm', ['install']);
-	console.log('Dependencies installed successfully.');
-	console.log();
+		// --- Install dependencies
+		console.log('Installing dependencies...');
+	//	spawnSync('npm install');
+		await run_cmd('npm', ['install']);
+		console.log('Dependencies installed successfully.');
+		console.log();
 
-	// --- Clean unused files
-	console.log('Removing useless files');
-//	spawnSync('npx rimraf ./.git');
-	run_cmd('npx', ['rimraf','./.git']);
-	fs.rmdirSync(path.join(newDirPath, 'bin'), {recursive: true});
+		// --- Clean unused files
+		console.log('Removing useless files');
+	//	spawnSync('npx rimraf ./.git');
+		await run_cmd('npx', ['rimraf','./.git']);
+		fs.rmdirSync(path.join(newDirPath, 'bin'), {recursive: true});
 
-	console.log('The installation is done, this is ready to use !');
-	}
-catch (error) {
-	console.log(error);
-	}
+		console.log('The installation is done, this is ready to use !');
+		}
+	catch (error) {
+		console.log(error);
+		}
+	} // main()
+
+main()
